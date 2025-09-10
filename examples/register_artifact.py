97b30d813631864697d0a0e3528fec9665c95e2e
awolegechu <awolegechu@gmail.com>
2025-09-10 20:07:54 +0800

    fix(gs): fix replica repository sql errors

--- examples/register_artifact.py
+++ examples/register_artifact.py
@@ -10,7 +10,7 @@ from tensorcast.api import register_artifact
 from tensorcast.api._config import RegisterArtifactOptions
 from tensorcast.types import ArtifactDescriptor
 
-init()
+init(daemon_port=50052, connect_only=True)
 
 hf_model_name = "Qwen/Qwen3-0.6B"
 # Load a artifact from HuggingFace artifact hub.
