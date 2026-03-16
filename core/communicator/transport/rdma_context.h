6b196f6d448f94660947d67a4a118e9d0f9224b3
wolegechu <ychu.luo@gmail.com>
2026-03-16 14:52:09 +0800

    feat(communicator): add RDMA benchmark binary and transport updates

--- core/communicator/transport/rdma_context.h
+++ core/communicator/transport/rdma_context.h
@@ -62,6 +62,10 @@ class RdmaContext {
     qp_retry_ = qp_retry;
   }
 
+  void set_outstanding_wr(int outstanding_wr) {
+    outstanding_wr_ = outstanding_wr;
+  }
+
   // Multi-QP configuration
   void set_multi_qp_config(int qp_count, bool bonding_balance) {
     qp_count_ = qp_count;
@@ -84,6 +88,10 @@ class RdmaContext {
     return qp_count_;
   }
 
+  int outstanding_wr() const {
+    return outstanding_wr_;
+  }
+
   bool bonding_balance() const {
     return bonding_balance_;
   }
@@ -99,6 +107,7 @@ class RdmaContext {
   int qp_timeout_ = 20;
   int qp_retry_ = 7;
   int qp_count_ = 1;
+  int outstanding_wr_ = 64;
   bool bonding_balance_ = false;
 };
 
